5f735b0f76815a04c644c169eec6e56876779a3e
Azmayen Fayek Sabil <azmayensabil@gmail.com>
2025-08-11 01:17:01 +0600

    Update sequelize.js

--- config/sequelize.js
+++ config/sequelize.js
@@ -4,6 +4,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('clinic', 'root', 'admin', {
   host: 'mysql.railway.internal',
   dialect: 'mysql',
+  port: 3306,
 //   logging: false, // remove this line if you want SQL logs
 });
 
